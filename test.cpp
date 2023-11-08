64e51ed1af75f69155c7bb16ca2f4d254d2c2844
Trần Nguyên <nguyen3503@tdkn3.local>
2023-11-08 10:28:34 +0300

    test

--- test.cpp
+++ test.cpp
@@ -2,9 +2,5 @@
 using namespace std;
 
 int main(){
-    cout << " Hello World";
-    cout << "Nguyen tran dep trai";
-    cout << "Nguyen tran ko dep trai";
-    cout << "ALoooo";
-    return 0;
+  cout << "hello world";
 }
